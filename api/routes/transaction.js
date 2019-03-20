a8ad86d1f79807606bc0838d759f34c1b81d4309
Andrew Chen <andrew.chen@scotiabank.com>
2019-03-20 11:13:14 -0400

    added rmethod populate researchtype

--- api/routes/transaction.js
+++ api/routes/transaction.js
@@ -20,6 +20,36 @@ module.exports = function (router) {
 
     Transaction.find(qry)
       .sort({ 'transactionDate': 1 })
+      .populate('researchType')
+      .exec()
+      .then(docs => res.status(200)
+        .json(docs))
+      .catch(err => res.status(500)
+        .json({
+          message: 'Error finding transactions for user',
+          error: err
+        }))
+  })
+
+  // test to populate researchtypes
+  router.get('/transactions/:year/:month', function (req, res) {
+    const userId = req.get('userId')
+    const month = req.params.month - 1 // JS months are zero-based
+    const year = req.params.year
+    const startDt = new Date(Date.UTC(year, month, 1, 0, 0, 0))
+    const endDt = new Date(Date.UTC(year, month + 1, 1, 0, 0, 0))
+
+    const qry = {
+      userId: userId,
+      transactionDate: {
+        $gte: startDt,
+        $lt: endDt
+      }
+    }
+
+    Transaction.find(qry)
+      .sort({ 'transactionDate': 1 })
+      .populate('researchType')
       .exec()
       .then(docs => res.status(200)
         .json(docs))
--- models/transaction.js
+++ models/transaction.js
@@ -11,7 +11,7 @@ let transactionSchema = new Schema({
   deposit: { type: Number, default: 0 },
   notes: { type: String, default: '' },
   createdOn: { type: Date, default: Date.now },
-  requestType: { type: Schema.Types.ObjectId, ref: 'Researchtype' }
+  researchType: { type: Schema.Types.ObjectId, ref: 'Researchtype' }
 })
 
 const Transaction = mongoose.model('Transaction', transactionSchema)
