cefd8f984c9c356068c0f91213b394fccd43d4c0
Andrew Chen <andrew.chen@scotiabank.com>
2019-03-20 13:43:13 -0400

    finish restype

--- models/transaction.js
+++ models/transaction.js
@@ -4,14 +4,14 @@ const Schema = mongoose.Schema
 let transactionSchema = new Schema({
   userId: mongoose.SchemaTypes.ObjectId,
   transactionDate: { type: Date, required: true },
-  transactionType: { type: String, required: true },
-  feedback: { type: String, default: 'Good job :-)' },
+  transactionType: { type: String, required: false },
+  feedback: { type: String, default: 'no feedback yet.' },
   description: { type: String, required: true },
   charge: { type: Number, default: 0 },
   deposit: { type: Number, default: 0 },
   notes: { type: String, default: '' },
   createdOn: { type: Date, default: Date.now },
-  researchType: { type: Schema.Types.ObjectId, ref: 'Researchtype' }
+  researchType: { type: Schema.Types.ObjectId, ref: 'Researchtype', required: true }
 })
 
 const Transaction = mongoose.model('Transaction', transactionSchema)
